f06d5a736514765c3e7971ecdcecdd0246949b52
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-07-29 15:22:16 +0430

    add the any method for enumerable module

--- MyEnumerable.rb
+++ MyEnumerable.rb
@@ -5,4 +5,12 @@ module MyEnumerable
       each { |n| return false unless yield (n) }
       true
     end
+
+    def any?
+      return true unless block_given?
+
+      each { |n| return true if yield (n) }
+      false
+    end
 end
+
